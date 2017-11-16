11695f33ab9e31a047b772a2585eba9548c84848
aswerty2011 <947235799@qq.com>
2017-11-16 20:01:16 +0800

    更新productInfoSql

--- doc/init.sql
+++ doc/init.sql
@@ -17,6 +17,7 @@ CREATE TABLE `product_info`(
 	`product_description` VARCHAR(64) COMMENT '描述',
 	`product_icon` VARCHAR(512) COMMENT '小图',
 	`category_type` INT NOT NULL COMMENT '类目编号',
+	`product_status` INT NOT NULL COMMENT '状态 0正常1下架',
 	`create_time` TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT '创建时间',
 	`update_time` TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP COMMENT '修改时间',
 	PRIMARY KEY (`product_id`)
